ed22e457534358eb7edfdf31184053c02bc924d2
aishrath <ashrthefirst@gmail.com>
2019-06-17 01:25:51 -0400

    Added JFrame

--- JCalculator/JCalc.java
+++ JCalculator/JCalc.java
@@ -1,5 +1,6 @@
 package JCalculator;
 
+import javax.swing.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,12 +11,35 @@ public class JCalc {
     private String calculatorTitle;
     private final List<Integer> keypadNumbers;
     private final List<Character> keypadOperators;
+    private JFrame JCalcFrame;
+
 
     JCalc() {
         calculatorTitle = DEFAULT_CALCULATOR_TITLE;
         keypadNumbers = new ArrayList<>();
         keypadOperators = new ArrayList<>();
+        JCalcFrame = new JFrame();
         initializeKeyPad(keypadNumbers, keypadOperators);
+        JTextPane jTextPane = new JTextPane();
+        jTextPane.setBounds(5, 0, 390, 80);
+        JCalcFrame.add(jTextPane);
+        generateOperatorsOnKeyPad();
+
+        JCalcFrame.setSize(400, 500);
+        JCalcFrame.setLayout(null);
+        JCalcFrame.setVisible(true);
+    }
+
+    private void generateOperatorsOnKeyPad() {
+        int start_pos_x = 290;
+        int start_pos_y = 50;
+
+        for (Object key : keypadOperators) {
+            System.out.println("Adding Operator Key...: " + key.toString());
+            JButton b = new JButton(key.toString());
+            b.setBounds(start_pos_x, start_pos_y + 50, 100, 40);
+            JCalcFrame.add(b);
+        }
     }
 
     public String getCalculatorTitle() {
